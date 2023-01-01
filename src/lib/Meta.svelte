257684bf72097bb5c51b553f53c4d683d5207306
tbhmens <57283066+tbhmens@users.noreply.github.com>
2023-01-01 05:06:18 +0100

    fix meta

--- src/lib/Meta.svelte
+++ src/lib/Meta.svelte
@@ -2,10 +2,12 @@
 	export let title: string
 	export let description = "the esthe site :)"
 	export let type: "article" | "website" = "website"
+	import { page } from "$app/stores"
 </script>
 
 <svelte:head>
 	<title>{title}</title>
+	<meta property="og:url" content={`https://esthe.live${$page.url.pathname}`} />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 	<meta property="og:type" content={type} />
--- src/routes/posts/+layout.svelte
+++ src/routes/posts/+layout.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
 	import { page } from "$app/stores"
-	import { posts } from "./posts.js"
+	import { posts, type Post } from "./posts.js"
 	import Box from "$lib/Box.svelte"
 	import Meta from "$lib/Meta.svelte"
 	import Homepage from "../../lib/Homepage.svelte"
 	import "./prism-onedark.css"
 	$: path = $page.url.pathname
-	$: meta = posts.find((post) => post.path === path)
+	$: meta = posts.find((post) => post.path === path) as Post
 </script>
 
+<Meta title={meta.title} description={meta.description} type="article" />
 <svelte:head>
-	{#if meta != null}
-		<Meta title={meta.title} description={meta.description} type="article" />
-		<meta property="article:published_time" content={meta.ymd} />
-		<meta property="article:author" content="esthe" />
-	{:else}
-		<Meta title="404" description="Page not found" />
-	{/if}
+	<meta property="article:published_time" content={meta.ymd} />
+	<meta property="article:author" content="esthe" />
 </svelte:head>
 
 <Box tag="main" align="start" class="markdown">
